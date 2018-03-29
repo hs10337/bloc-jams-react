4368d97993d100c0b1a9feea04b4a2d9d8b5e1a0
hs10337 <hs10337@gmail.com>
2018-03-29 06:54:47 -0700

    Add buttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar'
 
 class Album extends Component {
   constructor(props) {
@@ -44,6 +45,22 @@ class Album extends Component {
     }
   }
 
+  handlePrevClick(song) {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
+  }
+
+  handleNextClick(song) {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = currentIndex + 1;
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
+  }
+
   render() {
     return(
       <section className="album">
@@ -77,6 +94,12 @@ class Album extends Component {
             )}
             </tbody>
         </table>
+        <PlayerBar
+          isPlaying={this.state.isPlaying}
+          currentSong={this.state.currentSong}
+          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+          handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()} />
       </section>
     );
   }
